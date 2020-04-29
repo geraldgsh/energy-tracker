e7efbd5dcf6f05c3ea3554e32c290e38f9218edd
geraldgsh <geraldgsh@hotmail.com>
2020-04-30 00:12:11 +0800

    completed bottom navbar

--- app/javascript/containers/Home.jsx
+++ app/javascript/containers/Home.jsx
@@ -98,24 +98,32 @@ export default () => (
       </div>
       <footer id="sticky-footer" className="bg-grey fixed-bottom">
         <div className="container text-center">
-          <div className="row">
-            <div className="col foot-col">
-              <i className="fas fa-chart-bar"></i>
-              <p>Add Readings</p>
-            </div>
-            <div className="col foot-col">
-              <i className="fas fa-chart-line"></i>
-              <p>Track.it</p>
-            </div>
-            <div className="col foot-col">
-              <i className="fas fa-chart-pie"></i>
-              <p>Report</p>
-            </div>
-            <div className="col foot-col">
-              <i className="fas fa-ellipsis-h"></i>
-              <p>More</p>
-            </div>
-          </div>
+          <ul className="row foot-row nav nav-pills tablist" role="tablist">
+            <li className="col foot-col nav-item">
+              <a className="nav-link" data-toggle="pill" href="#readings">
+                <i className="fas fa-chart-bar"></i>
+                <p>Add Readings</p>
+              </a>              
+            </li>
+            <li className="col foot-col nav-item">
+              <a className="nav-link" data-toggle="pill" href="#track">
+                <i className="fas fa-chart-line"></i>
+                <p>Track.it</p>
+              </a>              
+            </li>
+            <li className="col foot-col nav-item">
+              <a className="nav-link" data-toggle="pill" href="#report">
+                <i className="fas fa-chart-pie"></i>
+                <p>Report</p>
+              </a>              
+            </li>
+            <li className="col foot-col nav-item">
+              <a className="nav-link" data-toggle="pill" href="#more">
+                <i className="fas fa-ellipsis-h"></i>
+                <p>More</p>
+              </a>
+            </li>
+          </ul>
         </div>
       </footer>
     </div>
