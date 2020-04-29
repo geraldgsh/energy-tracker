a3f053a20f58b0a5d97255f1bb281db64cd31684
geraldgsh <geraldgsh@hotmail.com>
2020-04-30 06:53:22 +0800

    added preference columns to user table

---
+++ db/migrate/20200429224210_add_preference_to_users.rb
@@ -0,0 +1,7 @@
+class AddPreferenceToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :units, :string
+    add_column :users, :target, :string
+    add_column :users, :string, :string
+  end
+end
