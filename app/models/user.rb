e7ed4aa86de63a199535f84911cd2d8d60cc5c61
geraldgsh <geraldgsh@hotmail.com>
2020-05-06 03:09:30 +0800

    migrated user id reading table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :readings
   validates :name, presence: true, length: { minimum: 3, maximum: 100 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :units, presence: true
--- db/migrate/20200505184926_create_readings.rb
+++ db/migrate/20200505184926_create_readings.rb
@@ -1,24 +1,15 @@
 class CreateReadings < ActiveRecord::Migration[6.0]
   def change
     create_table :readings do |t|
-      t.string :bedroom
-      t.string :integer
-      t.string :study
-      t.string :integer
-      t.string :garage
-      t.string :integer
-      t.string :living
-      t.string :integer
-      t.string :kitchen
-      t.string :integer
-      t.string :guest
-      t.string :integer
-      t.string :consumption
-      t.string :integer
-      t.string :available
-      t.string :integer
-      t.string :saved
-      t.string :integer
+      t.integer :bedroom
+      t.integer :study
+      t.integer :garage
+      t.integer :living
+      t.integer :kitchen
+      t.integer :guest
+      t.integer :consumption
+      t.integer :available
+      t.integer :saved
 
       t.timestamps
     end
